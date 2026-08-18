34216df2761b52ac8677d1eed538d0611b3040d7
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-08-18 03:03:21 +0200

    added demo file and cleaned up package.json

---
+++ package/demo/index.js
@@ -0,0 +1,20 @@
+import { defineSchema } from "../src/core/defineSchema";
+
+const UserSch = defineSchema("User", {
+    name: "string",
+    age: "number"
+});
+console.log("Demonstrating with User schema: ");
+console.log(UserSch);
+
+console.log("Demo #1: This input should be VALID")
+console.log(UserSch.validate({
+    name: "Vedansh",
+    age: 12
+}));
+
+console.log("Demo #2: This input should NOT be valid");
+console.log(UserSch.validate({
+    "name": "Vedansh",
+    "age": "12"
+}))
