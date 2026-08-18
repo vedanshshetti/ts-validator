a624e2666ebf3c19edaf7f980b766b68be294493
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-08-18 18:05:19 +0200

    v1.4.0 - removed types from namespace + add prepublishScript

--- package/src/index.ts
+++ package/src/index.ts
@@ -2,7 +2,5 @@ import { defineSchema } from "./core/defineSchema";
 import type { InferSchemaType, SchemaInterface as SchInt } from "./types";
 export const tsv = { defineSchema };
 
-export namespace Types {
-  export type InferSchema<T extends SchemaInterface> = InferSchemaType<T>;
-  export type SchemaInterface = SchInt;
-}
+export type InferSchema<T extends SchemaInterface> = InferSchemaType<T>;
+export type SchemaInterface = SchInt;
