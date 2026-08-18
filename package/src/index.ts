b66c8a322d69aa0efb37bb1a7934ad6f6d013eec
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-08-18 17:34:52 +0200

    v1.3.0 - added InferSchema and SchemaInterface types to main export

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,2 +1,8 @@
 import { defineSchema } from "./core/defineSchema";
+import type { InferSchemaType, SchemaInterface as SchInt } from "./types";
 export const tsv = { defineSchema };
+
+export namespace Types {
+  export type InferSchema<T extends SchemaInterface> = InferSchemaType<T>;
+  export type SchemaInterface = SchInt;
+}
--- package/src/types/index.ts
+++ package/src/types/index.ts
@@ -14,3 +14,5 @@ export type TypeofOptions =
     | "array" 
     | "undefined" 
     | "function";
+
+export type InferSchemaType<T extends SchemaInterface> = T;
